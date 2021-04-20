5d49f2d5d26aec20bf0614de1dba94b34778352e
JessicaAquino <jessica.aquino.torrez@gmail.com>
2021-04-19 20:26:42 -0400

    Add project files.

---
+++ PrepParcial2CMyU/apiProductor/Controllers/DataController.cs
@@ -0,0 +1,39 @@
+﻿using apiProductor.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Azure.Messaging.ServiceBus;
+
+namespace apiProductor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<bool> EnviarAsync([FromBody] Data data)
+        {
+            string connectionString = "Endpoint=sb://raspberrysb.servicebus.windows.net/;SharedAccessKeyName=EnviarEscuchar;SharedAccessKey=ffV9DPVNTiipDvn3aVuUNZ1pB78tT/3+yObJHc7WnLU=;EntityPath=raspberryqueue";
+            string queueName = "raspberryqueue";
+            string mensaje = JsonConvert.SerializeObject(data);
+            await using (ServiceBusClient client = new ServiceBusClient(connectionString))
+            {
+                // create a sender for the queue 
+                ServiceBusSender sender = client.CreateSender(queueName);
+
+                // create a message that we can send
+                ServiceBusMessage message = new ServiceBusMessage(mensaje);
+
+                // send the message
+                await sender.SendMessageAsync(message);
+                Console.WriteLine($"Sent a single message to the queue: {queueName}");
+            }
+            return true;
+        }
+    }
+}
---
+++ PrepParcial2CMyU/apiProductor/Models/Data.cs
@@ -0,0 +1,18 @@
+﻿namespace apiProductor.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Data
+    {
+        [Key]
+        public int MessageID { get; set; }
+        [Required]
+        public double Temperature { get; set; }
+        [DataType(DataType.DateTime)]
+        [Required]
+        public DateTime TempDate { get; set; }
+        [Required]
+        public double Humidity { get; set; }
+
+    }
+}
